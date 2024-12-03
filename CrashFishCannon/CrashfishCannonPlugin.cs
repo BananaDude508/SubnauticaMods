1cdebf958c6bec48ac4b75781e48a58ba8d1c692
BananaDude508 <malsebhal@gmail.com>
2024-12-03 17:38:34 +1030

    Crash Fish Cannon

---
+++ CrashFishCannon/CrashfishCannonPlugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace BananaDude508.CrashFishCannon
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    [BepInDependency("com.snmodding.nautilus")] // marks Nautilus as a dependency for this mod
+    public class CrashFishCannonPlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.bananadude508.crashfishcannon";
+        private const string PluginName = "Crash Fish Cannon";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
---
+++ CrashFishCannon/PropulsionCannonPatches.cs
@@ -0,0 +1,37 @@
+﻿using BananaDude508.CrashFishCannon;
+using HarmonyLib;
+using Nautilus.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BananaDude508.CrashFishCannon
+{
+    [HarmonyPatch(typeof(PropulsionCannon))]
+    internal class PropulsionCannonPatches
+    {
+        [HarmonyPatch(nameof(PropulsionCannon.Filter))]
+        [HarmonyPrefix]
+        public static bool Filter_Prefix(PropulsionCannon __instance, InventoryItem item, ref bool __result)
+        {
+            if (item == null || item.item == null || __instance.transform.IsChildOf(item.item.GetComponent<Transform>()))
+            {
+                __result = false;
+                return false;
+            }
+            List<TechType> cannonAmmoWhitelist = new List<TechType>()
+            {
+                TechType.Crash
+            };
+
+            TechType techType = item.item.GetTechType();
+            if (cannonAmmoWhitelist.Contains(item.techType))
+            {
+                __result = true;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
